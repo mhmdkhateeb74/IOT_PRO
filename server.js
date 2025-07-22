0beb6d503f9d05d2191448f79b3335b648070537
mhmdkhateeb74 <mhmdkhateeb92@gmail.com>
2025-07-22 13:56:25 +0300

    WebSocket server setups
    express ו-http משמשים ליצירת שרת HTTP המארח את הממשק הקדמי (Frontend).
    ws היא ספריית Node.js ליצירת WebSocket לתקשורת בזמן אמת עם המכשירים.
    clients הוא מפה (Map) בה נשמרים המכשירים המחוברים יחד עם מזהה ה-id שלהם.
    הפורט נקבע כ-4020 שעליו השרת מאזין.

---
+++ server.js
@@ -0,0 +1,12 @@
+const WebSocket = require("ws");
+const express = require("express");
+const path = require("path");
+const http = require("http");
+
+const httpPort = 4020;
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const clients = new Map();
